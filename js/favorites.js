8836d558fa5a06dfa7486a146fba70d29d3e1b08
valdeirbarbosa <valdeir.barbosa@outlook.com>
2023-04-27 23:44:31 -0300

    ajustes de mobile e cores

--- js/favorites.js
+++ js/favorites.js
@@ -73,10 +73,11 @@ export class FavoritesView extends Favorites {
       row.querySelector('.card-body  img').src = `https://github.com/${user.login}.png`
       // row.querySelector('.card-title a').href = `https://github.com/${user.login}`
       // row.querySelector('.user img').alt = `Imagem do perfil do ${user.name}`
-      row.querySelector('.card-title h3').textContent = user.name
-      row.querySelector('.card-title h4 ').textContent = `${user.login}`
-      row.querySelector('.git-profile span').textContent = `${user.login}`
-      row.querySelector('.card-body p').textContent = `${user.bio}`
+      row.querySelector('.card-title h3').textContent = user.name=== null?user.login:user.name
+      row.querySelector('.card-title h4 a').textContent = `${user.login}`
+      row.querySelector('.git-profile span a ').textContent = `${user.login}`
+      row.querySelector('.git-profile span a ').href = `https://github.com/${user.login}`
+      row.querySelector('.card-body p').textContent = `${user.bio !== null ? user.bio : "₢"}`
       row.querySelector('.repo span').textContent = user.public_repos
       row.querySelector('.followers span').textContent = user.followers
       row.querySelector('.card-title svg').onclick = () => {
@@ -153,7 +154,7 @@ export class FavoritesView extends Favorites {
                 d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
               ></path>
             </svg>
-            <span>maykbrito</span>
+             <span> <a href="http://github.com/maykbrito" target="_blank">maykbrito</a></span>
           </div>
           <div class="repo">
             <svg height="32" viewBox="0 0 16 16" version="1.1" width="32">
